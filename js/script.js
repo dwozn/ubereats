5eb381eb4a87d0c068f44b94a6d589bd795cf1ef
dwozn <domwozn97@gmail.com>
2023-07-15 15:12:21 +0200

    Added desktop view

--- js/script.js
+++ js/script.js
@@ -6,8 +6,22 @@ document.addEventListener("DOMContentLoaded", function(){
         menu_toggled = false,
         delivery_adress_wrapper = document.querySelector(".delivery-adress-wrapper"),
         sticky_offset = delivery_adress_wrapper.offsetTop,
-        delivery_adress_wrapper_filler = document.querySelector(".delivery-adress-wrapper-filler");
+        delivery_adress_wrapper_filler = document.querySelector(".delivery-adress-wrapper-filler"),
+        menu = document.querySelector(".menu"),
+        navbar_input = document.querySelector("input.delivery-adress-description"),
+        navbar_search_box = document.querySelector(".delivery-adress-wrapper-input.desktop-view");
 
+    
+    //line after focus
+    navbar_input.addEventListener("focus", function(){
+        navbar_search_box.classList.add('focus');
+    });
+
+    navbar_input.addEventListener("blur", function(){
+        navbar_search_box.classList.remove('focus');
+    });   
+    
+    //toggle main menu
     main_menu_button.addEventListener("click", function(){
         navigation_container.classList.add('active');
         navigation_wrapper.classList.add('active');
@@ -24,16 +38,36 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
 
-    window.onscroll = function(){ stickySearchBar() };
+    window.onscroll = function(){ scrollEvent() };
 
-    function stickySearchBar() {
-        if (( window.scrollY - 72 ) >= sticky_offset) {
-            delivery_adress_wrapper.classList.add('sticky');
-            delivery_adress_wrapper_filler.classList.remove('hidden');
+    function scrollEvent() {
+        if (window.innerWidth < 1080) { // for mobile view
+            
+            //sticky search bar for mobile view
+            if (window.scrollY >= sticky_offset) {
+                delivery_adress_wrapper.classList.add('sticky');
+                delivery_adress_wrapper_filler.classList.remove('hidden');
+            }
+            else {
+                delivery_adress_wrapper.classList.remove('sticky');
+                delivery_adress_wrapper_filler.classList.add('hidden');
+            }
         }
-        else {
-            delivery_adress_wrapper.classList.remove('sticky');
-            delivery_adress_wrapper_filler.classList.add('hidden');
+        else { //for desktop view
+            //hightlight navbar 
+            if ( window.scrollY >= 96 ) { // (96 - navbar height)
+                menu.classList.add('highlighted');
+            }
+            else {
+                menu.classList.remove('highlighted');
+            }
+            //show input box
+            if (window.scrollY >= sticky_offset) {
+                navbar_search_box.style.opacity = "1";
+            }
+            else {
+                navbar_search_box.style.opacity = "0";
+            }
         }
     }
 });
